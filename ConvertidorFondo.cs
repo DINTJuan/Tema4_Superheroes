2cae43ac994c4e1890761fb5adab3da4e8c00fdd
alumno <alumno@A302PC26>
2022-10-31 14:56:22 +0100

    Agregar archivos de proyecto.

---
+++ ConvertidorFondo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Tema4_Superheroes
+{
+    class ConvertidorFondo : IValueConverter
+    {
+    }
+}
---
+++ Superheroes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema4_Superheroes
+{
+    class Superheroes
+    {
+        public string Nombre { get; set; }
+        public string Imagen { get; set; }
+        public bool Vengador { get; set; }
+        public bool Xmen { get; set; }
+        public bool Heroe { get; set; }
+
+        public Superheroes()
+        {
+        }
+
+        public Superheroes(string nombre, string imagen, bool vengador, bool xmen, bool heroe)
+        {
+            Nombre = nombre;
+            Imagen = imagen;
+            Vengador = vengador;
+            Xmen = xmen;
+            Heroe = heroe;
+        }
+
+        public static List<Superheroes> GetSamples()
+        {
+            List<Superheroes> ejemplos = new List<Superheroes>();
+
+            Superheroes ironman = new Superheroes("Ironman", @"https://dossierinteractivo.com/wp-content/uploads/2021/01/Iron-Man.png", true, false, true);
+            Superheroes kingpin = new Superheroes("Kingpin", @"https://www.comicbasics.com/wp-content/uploads/2017/09/Kingpin.jpg", false, false, false);
+            Superheroes spiderman = new Superheroes("Spiderman", @"https://wipy.tv/wp-content/uploads/2019/08/destino-de-%E2%80%98Spider-Man%E2%80%99-en-los-Comics.jpg", true, true, true);
+
+            ejemplos.Add(ironman);
+            ejemplos.Add(kingpin);
+            ejemplos.Add(spiderman);
+
+            return ejemplos;
+        }
+    }
+}
